d25a411681cf95608c5ffa08dc8889443c412b1d
DaemonLife <52444457+DaemonLife@users.noreply.github.com>
2021-12-02 21:38:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from PIL import Image, ImageOps
 import os
 
+
+IMAGE_SIZE = 1080
+BORDER_SIZE = 20
+
 arr = os.listdir()
 
 folder_exist = False
@@ -12,10 +16,13 @@ for line in arr:
 if folder_exist == False:
     os.makedirs("Square")
 
+i = 0
+
 for line in arr:
     if line[len(line)-4:] == (".jpg" or ".png"):
-
+        i += 1
         # open image
+        print(f"{i}: Working with {line}")
         img = Image.open(line)
 
         # border color
@@ -25,24 +32,26 @@ for line in arr:
 
         if x > y:
             # * landscape
-            new_x = 1060
+            new_x = IMAGE_SIZE - 2*BORDER_SIZE 
             new_y = int(y*new_x/x)
             size=(new_x,new_y)
             new_img = img.resize(size)
-            border = (10, int((1080-new_y)/2), 10, int((1080-new_y)/2))
+            border = (BORDER_SIZE, int((IMAGE_SIZE-new_y)/2), BORDER_SIZE, int((IMAGE_SIZE-new_y)/2))
             new_img = ImageOps.expand(new_img, border=border, fill=color)
 
         else:
             # * portret or square
-            new_y = 1060
+            new_y = IMAGE_SIZE - 2*BORDER_SIZE
             new_x = int(x*new_y/y)
             size=(new_x,new_y)
             new_img = img.resize(size)
-            border = (int((1080-new_x)/2), 10, int((1080-new_x)/2), 10)
+            border = (int((IMAGE_SIZE-new_x)/2), BORDER_SIZE, int((IMAGE_SIZE-new_x)/2), BORDER_SIZE)
             new_img = ImageOps.expand(new_img, border=border, fill=color)
 
-        size=(1080,1080)
+        size=(IMAGE_SIZE,IMAGE_SIZE)
         new_img = new_img.resize(size)
 
         # save new image
         new_img.save("Square/"+line)
+        print(f"Done!")
+        print()
